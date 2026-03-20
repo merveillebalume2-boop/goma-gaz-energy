fb3927512dcb07731631dfc6cd59c96a8cc36ca0
unknown <https://github.com/merveillebalume2-boop/goma-gaz-energy.git>
2026-03-20 05:59:55 -0700

    Correction bug de demarrage Express

--- server/index.ts
+++ server/index.ts
@@ -116,7 +116,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Pour toutes les autres requêtes qui ne sont pas des API, renvoyer l'application React
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
